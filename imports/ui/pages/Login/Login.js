d8efa8431a47c1198b5b5dab686b6088cf422572
Arles96 <arles.cerrato@unitec.edu>
2019-01-29 00:18:07 -0600

    Agregando un diseño mas o menos al login

--- imports/ui/pages/Login/Login.js
+++ imports/ui/pages/Login/Login.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import NavBar from '../../components/Navbar';
+import { Form, Container, Header, Icon, Button, Message } from 'semantic-ui-react';
 
 // import components
 import Alert from '../../components/Alert';
@@ -13,9 +15,7 @@ class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
-      errMsg: null,
+      err: '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -36,86 +36,46 @@ class Login extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const { email, password } = this.state;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
     Meteor.loginWithPassword(email, password, err => {
       if (err) {
-        this.setState({ errMsg: err.reason });
-        return console.log(err);
+        this.setState({ err: err.reason });
       }
     });
   }
+
   render() {
     if (this.props.loggedIn) {
       return null;
     }
 
-    const { errMsg } = this.state;
+    const { err } = this.state;
     return (
-      <section className="login-page">
-        <div className="card mx-auto" style={{ maxWidth: '28rem' }}>
-          <div className="card-header">
-            <div className="brand">
-              <div className="text-center">
-                <img
-                  className="rounded-circle"
-                  src="https://via.placeholder.com/150x150"
-                  alt="logo"
-                />
-              </div>
+      <div id="loginSection">
+        <NavBar />
+        <Container>
+          <br />
+          <Header as="h2" icon textAlign="center" >
+            <Icon name="user" circular />
+            Iniciar Sesión
+          </Header>
+          <Form error onSubmit={this.handleSubmit} >
+            { err && <Message error content={err} /> }
+            <Form.Field>
+              <label>Correo</label>
+              <input type="email" name="email" />
+            </Form.Field>
+            <Form.Field>
+              <label>Contraseña</label>
+              <input type="password" name="password" />
+            </Form.Field>
+            <div className="text-center" >
+              <Button inverted color="blue" className="btn-signin">Iniciar Sesión</Button>
             </div>
-            <div className="card-body">
-              <h4 className="card-title">Login</h4>
-              <form onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                  <label htmlFor="email">E-Mail Address</label>
-
-                  <input
-                    id="email"
-                    type="email"
-                    className="form-control"
-                    name="email"
-                    value={this.state.email}
-                    onChange={e => this.setState({ email: e.target.value })}
-                    required
-                  />
-                </div>
-
-                <div className="form-group">
-                  <div className="spread-container">
-                    <label htmlFor="password">Password</label>
-                  </div>
-                  <input
-                    id="password"
-                    type="password"
-                    className="form-control"
-                    name="password"
-                    value={this.state.password}
-                    onChange={e => this.setState({ password: e.target.value })}
-                    required
-                  />
-                  <NavLink to="/recover-password">Forgot Password?</NavLink>
-                </div>
-                <div className="form-group no-margin">
-                  <button
-                    type="submit"
-                    className="btn btn-primary btn-block mb-2"
-                  >
-                    Login
-                  </button>
-                  {errMsg && <Alert errMsg={errMsg} />}
-                </div>
-                <div className="margin-top20">
-                  Don&apos;t have an account?{' '}
-                  <NavLink to="/signup">Create one</NavLink>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="footer text-center">
-            &copy; {new Date().getFullYear()}
-          </div>
-        </div>
-      </section>
+          </Form>
+        </Container>
+      </div>
     );
   }
 }
