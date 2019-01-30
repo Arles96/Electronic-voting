eaf610053924b04a55bac07f30b8ebff6d8e5451
Arles96 <arles.cerrato@unitec.edu>
2019-01-29 23:42:32 -0600

    reset password

--- imports/ui/pages/ResetPassword/ResetPassword.js
+++ imports/ui/pages/ResetPassword/ResetPassword.js
@@ -5,24 +5,57 @@ import NavBar from '../../components/Navbar/';
 
 import './ResetPassword.scss';
 
-const RecoverPassword = () => (
-  <div className="recover-password-page">
-    <NavBar />
-      <Container>
-        <Header textAlign="center" className="header-rpwd" >
-          Restablecer tu contraseña
-        </Header>
-        <Form onSubmit={this.handleSubmit} >
-          <Form.Field>
-            <label>Correo:</label>
-            <input type="email" name="email" />
-          </Form.Field>
-          <Form.Field className="text-center" >
-            <Button className="btn-recover" inverted color="green" >Enviar</Button>
-          </Form.Field>
-        </Form>
-      </Container>
-  </div>
-);
+class RecoverPassword extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      err: ''
+    };
+    console.log(this.props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    const password = e.target.password.value;
+    const passwordAgain = e.target.passwordAgain.value;
+    if (password === passwordAgain) {
+      Accounts.resetPassword('', password, (err) => {
+        if (err) {
+          this.setState({ err: err.reason });
+        }
+      });
+    } else {
+      this.setState({ err: 'Contraseñas no coinciden.' })
+    }
+  }
+
+  render () {
+    return (
+      <div className="recover-password-page">
+        <NavBar />
+          <Container>
+            <Header textAlign="center" className="header-rpwd" >
+              Recuperar contraseña
+            </Header>
+            <Form onSubmit={this.handleSubmit} >
+              <Form.Field>
+                <label>Contraseña:</label>
+                <input type="password" name="password" />
+              </Form.Field>
+              <Form.Field>
+                <label>Confirmar Contraseña:</label>
+                <input type="password" name="passwordAgain" />
+              </Form.Field>
+              <Form.Field className="text-center" >
+                <Button className="btn-recover" inverted color="green" >Enviar</Button>
+              </Form.Field>
+            </Form>
+          </Container>
+    </div>
+    );
+  }
+}
 
 export default RecoverPassword;
