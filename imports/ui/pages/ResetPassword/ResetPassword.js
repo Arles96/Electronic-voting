3e3333b9e9d9ceb2d977da820fbfa056440302eb
Arles96 <arles.cerrato@unitec.edu>
2019-01-29 23:27:02 -0600

    guardando cambios

--- imports/ui/pages/ResetPassword/ResetPassword.js
+++ imports/ui/pages/ResetPassword/ResetPassword.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Container, Header, Form, Button } from 'semantic-ui-react';
+import { Accounts } from 'meteor/accounts-base';
+import NavBar from '../../components/Navbar/';
 
 import './ResetPassword.scss';
 
 const RecoverPassword = () => (
   <div className="recover-password-page">
-    <h1>Recover Password Page</h1>
+    <NavBar />
+      <Container>
+        <Header textAlign="center" className="header-rpwd" >
+          Restablecer tu contraseña
+        </Header>
+        <Form onSubmit={this.handleSubmit} >
+          <Form.Field>
+            <label>Correo:</label>
+            <input type="email" name="email" />
+          </Form.Field>
+          <Form.Field className="text-center" >
+            <Button className="btn-recover" inverted color="green" >Enviar</Button>
+          </Form.Field>
+        </Form>
+      </Container>
   </div>
 );
 
