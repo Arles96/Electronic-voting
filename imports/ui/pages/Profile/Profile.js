75f90c64bf17a487a2da3a021fad0885c52d9255
rasbi123 <46825972+rasbi123@users.noreply.github.com>
2019-01-30 21:58:43 -0600

    Actualizado

--- imports/ui/pages/Profile/Profile.js
+++ imports/ui/pages/Profile/Profile.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Card, Icon, Feed} from 'semantic-ui-react';
 
 
 import './Profile.scss';
@@ -26,10 +26,67 @@ class Profile extends React.Component {
     return (
       <div className="profile-page">
        <Grid>
-        <Grid.Column floated='left' width={4}>
-          <Image src='/images/wireframe/paragraph.png' />
+         <h1>Menu</h1>
+        <Grid.Column width={5}>
+          <Card>
+            <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
+
+          <Card>
+    <Card.Content>
+      <Card.Header>Recent Activity</Card.Header>
+    </Card.Content>
+    <Card.Content>
+      <Feed>
+        <Feed.Event>
+          <Feed.Label image='/images/avatar/small/jenny.jpg' />
+          <Feed.Content>
+            <Feed.Date content='1 day ago' />
+            <Feed.Summary>
+              You added <a>Jenny Hess</a> to your <a>coworker</a> group.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+
+        <Feed.Event>
+          <Feed.Label image='/images/avatar/small/molly.png' />
+          <Feed.Content>
+            <Feed.Date content='3 days ago' />
+            <Feed.Summary>
+              You added <a>Molly Malone</a> as a friend.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+
+        <Feed.Event>
+          <Feed.Label image='/images/avatar/small/elliot.jpg' />
+          <Feed.Content>
+            <Feed.Date content='4 days ago' />
+            <Feed.Summary>
+              You added <a>Elliot Baker</a> to your <a>musicians</a> group.
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+      </Feed>
+    </Card.Content>
+  </Card>
+
         </Grid.Column>
-        <Grid.Column floated='right' width={8}>
+        <hi>Hola Usuario</hi>
+        <Grid.Column width={11}>
           <Image src='/images/wireframe/paragraph.png' />
         </Grid.Column>
       </Grid>
