cede2a5d08ed539674543cd8b8c043fc5672bd22
Arles96 <arles.cerrato@unitec.edu>
2019-01-31 15:24:39 -0600

    Hola

--- imports/ui/pages/Profile/Profile.js
+++ imports/ui/pages/Profile/Profile.js
@@ -26,11 +26,26 @@ class Profile extends React.Component {
     return (
       <div className="profile-page">
        <Grid>
-        <Grid.Column floated='left' width={4}>
-          <Image src='/images/wireframe/paragraph.png' />
+        <Grid.Column className="bg-dark" width={5}>
+          <Card>
+          <Image src='/images/avatar/large/matthew.png' />
+          <Card.Content>
+            <Card.Header>Matthew</Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              22 Friends
+            </a>
+          </Card.Content>
+        </Card>
         </Grid.Column>
-        <Grid.Column floated='right' width={8}>
-          <Image src='/images/wireframe/paragraph.png' />
+        <Grid.Column className="bg-red"  width={11}>
+          <h1>Hola container</h1>
         </Grid.Column>
       </Grid>
       </div>
