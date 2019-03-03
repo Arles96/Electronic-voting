f38f7a430e6e67ab10154d6119466d8b3732559a
CJAlvarez <cristhianalvarez96@gmail.com>
2019-03-03 12:34:49 -0600

    After merge

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import 'semantic-ui-css/semantic.css'
 import 'react-datepicker/dist/react-datepicker.css';
-=======
-import 'semantic-ui-css/semantic.min.css'
->>>>>>> cae8e334be8a2cb5cd6d31b94bc4026e825083d0
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
